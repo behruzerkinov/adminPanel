87a928a32daae193c1096ea441b4c5b0afc029c7
behruzerkinov <erkinovbehruz0@gmail.com>
2024-11-11 00:07:32 +0500

    Everything. Just some bug fixes on rendering side

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -4,25 +4,7 @@ const apiKey =
    "https://documenter.getpostman.com/view/24974792/2s935oL3qs#a350b0bf-2888-4af0-aaa5-3111ce63dade";
 
 const Contents = () => {
-   const [content, setContent] = useState([]);
-
-   useEffect(() => {
-      const fetchContents = async () => {
-         try {
-            const response = await fetch(apiKey);
-            const data = await response.json();
-            setContent(data);
-         } catch (error) {
-            console.error("Error fetching contents:", error);
-         }
-      };
-
-      fetchContents();
-   }, []);
-
-   return <div>
-
-   </div>;
+   return <div></div>;
 };
 
 export default Contents;
